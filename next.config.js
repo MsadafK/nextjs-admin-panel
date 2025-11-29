aaf8e239feb9c761977e70d97b3536c5ac9a120e
MsadafK <mssadaf0@gmail.com>
2025-11-29 12:02:42 +0530

    update

--- next.config.js
+++ next.config.js
@@ -12,6 +12,7 @@ const nextConfig = {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
   },
+  eslint: { ignoreDuringBuilds: true }
 }
 
 module.exports = nextConfig
